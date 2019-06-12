fdf1e048b03e061453a734594dba2635bc8fa992
Samuel Heer <samuel.heer@diging.de>
2019-06-12 15:18:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Contao Bricks Header Bundle
+
+
+## Release
+
+Run the PHP-CS-Fixer and the unit test before you release your bundle:
+
+```bash
+vendor/bin/php-cs-fixer fix -v
+vendor/bin/phpunit
+```
+
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace ExAkt\ContaoBricksHeaderBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ExAkt\ContaoBricksHeaderBundle\ContaoBricksHeaderBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoBricksHeaderBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
