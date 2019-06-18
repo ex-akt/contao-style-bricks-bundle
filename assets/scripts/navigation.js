460520809e294b56cd007a60adbc87bc49057d89
Samuel Heer <samuel.heer@diging.de>
2019-06-18 15:14:29 +0200

    different header styles implemented

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,7 +1,7 @@
 var mainMenu = (function(){
     function toggleMenu(){
         var hamburgerIconClassList = document.getElementsByClassName("hamburger").item(0).classList;
-        var mainMenuClassList = document.getElementsByClassName("nav-head").item(0).classList;
+        var mainMenuClassList = document.getElementsByClassName("nav-bricks").item(0).classList;
 
         if(hamburgerIconClassList.contains('is-active')){
             hamburgerIconClassList.remove('is-active');
@@ -15,7 +15,7 @@ var mainMenu = (function(){
 
     document.getElementsByClassName('hamburger').item(0).addEventListener('click',toggleMenu);
 
-    var menuLinks = document.querySelectorAll('.nav-head a');
+    var menuLinks = document.querySelectorAll('.nav-bricks a');
     for (var link of menuLinks) {
         link.addEventListener('click', toggleMenu);
     }
