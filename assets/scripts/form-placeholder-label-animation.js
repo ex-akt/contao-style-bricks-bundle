dc1b4f1b91c2851cd75ba200f68bde0226b1b6bc
Samuel Heer <samuel.heer@diging.de>
2019-07-03 13:10:50 +0200

    minor changes

--- assets/scripts/form-placeholder-label-animation.js
+++ assets/scripts/form-placeholder-label-animation.js
@@ -16,3 +16,29 @@ for(i = 0; i < inputs.length; i++){
         }
     })
 }
+
+
+// Form field focus
+/*
+window.addEventListener('load', function() {
+
+    var el = document.querySelectorAll('footer .widget');
+    if (el[0]) {
+        let coords = getCoords(el[0]);
+        console.log(coords.top + ' - '+window.pageYOffset);
+        if (coords.top > window.pageYOffset) {
+            console.log('scroll');
+            window.scrollTo(0, coords.top - 120);
+        }
+    }
+});
+
+
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+}*/
