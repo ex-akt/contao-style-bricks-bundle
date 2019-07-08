899bf31fee1754f13065f0b80aff22465eaf4e20
Samuel Heer <samuel.heer@diging.de>
2019-07-08 16:54:09 +0200

    rewrite form-place-holder-label-animation

--- assets/scripts/form-placeholder-label-animation.js
+++ assets/scripts/form-placeholder-label-animation.js
@@ -1,44 +1,50 @@
-var inputs = document.querySelectorAll("footer .widget input, footer .widget textarea");
+import '../scss/forms/_forms.scss'; 
 
-for(i = 0; i < inputs.length; i++){
-
-    inputs[i].addEventListener("focus",function(){
-        this.parentElement.classList.add('focused');
-    });
-    inputs[i].addEventListener("blur", function(){
-        var value = this.value.trim();
-        if(value == ""){
-            this.classList.remove('filled');
-            this.parentElement.classList.remove('focused');
-        }
-        else{
-            this.classList.add('filled');
-        }
-    })
+function brick_checkForFilledWidget(element){
+    var value = element.value.trim();
+    if(value == ""){
+        element.classList.remove('filled');
+        element.parentElement.classList.remove('focused');
+    }
+    else{
+        element.classList.add('filled');
+        element.parentElement.classList.add('focused');
+    }
 }
 
-
-// Form field focus
-/*
-window.addEventListener('load', function() {
-
-    var el = document.querySelectorAll('footer .widget');
-    if (el[0]) {
-        let coords = getCoords(el[0]);
-        console.log(coords.top + ' - '+window.pageYOffset);
-        if (coords.top > window.pageYOffset) {
-            console.log('scroll');
-            window.scrollTo(0, coords.top - 120);
-        }
-    }
-});
-
-
-function getCoords(elem) {
+function brick_getCoords(elem) {
     let box = elem.getBoundingClientRect();
 
     return {
         top: box.top + pageYOffset,
         left: box.left + pageXOffset
     };
-}*/
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var inputs = document.querySelectorAll("footer .widget input, footer .widget textarea");
+
+    for (var i = 0; i < inputs.length; i++) {
+        var widget = inputs[i];
+
+        brick_checkForFilledWidget(widget);
+
+        //Label Events-Handling
+        widget.addEventListener("focus", function () {
+            this.parentElement.classList.add('focused');
+        });
+        widget.addEventListener("blur", function (event) {
+            brick_checkForFilledWidget(this);
+        });
+
+        //Error Handling (Scroll to form)
+        if(widget.classList.contains('error')){
+            let coords = brick_getCoords(widget)
+            if (coords.top > window.pageYOffset) {
+                window.scrollTo(0, coords.top - 120);
+            }
+            let errorExplanationWidget =  document.getElementsByClassName('error-message');
+            errorExplanationWidget.item(0).classList.remove('hidden');
+        }
+    }
+});
