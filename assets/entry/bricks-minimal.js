2325c791e85bda4531a08d1fd838bca3185dc3ee
Samuel Heer <samuel@ex-akt.de>
2020-02-19 15:17:44 +0100

    adjust minimal bricks entry

---
+++ assets/entry/bricks-minimal.js
@@ -0,0 +1,15 @@
+//Basic
+import '../scripts/polyfills';
+
+//Components
+//import '../scripts/slider';
+//import '../scripts/lightbox';
+//import '../scripts/navigation';
+
+//Ajax
+//import '../scripts/ajax-pagination';
+
+//Forms
+//import '../scripts/form-placeholder-label-animation';
+//import '../scripts/form-error';
+
