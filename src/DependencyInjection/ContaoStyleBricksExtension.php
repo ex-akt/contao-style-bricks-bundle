e13ede5f34154931ccecc2b7ff01a2cd3bafa565
Samuel Heer <samuel@ex-akt.de>
2023-06-21 10:06:48 +0200

    bugfix

--- src/DependencyInjection/ContaoStyleBricksExtension.php
+++ src/DependencyInjection/ContaoStyleBricksExtension.php
@@ -26,7 +26,6 @@ final class ContaoStyleBricksExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration(new Configuration(), $configs);
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
 
         $loader->load('services.yml');
